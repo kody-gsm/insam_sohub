b183485e39cf06b035bc77ce8d0b4bd316b4a90b
byundojin <103907292+byundojin@users.noreply.github.com>
2024-04-01 19:52:41 +0900

    이름 변경

---
+++ app/user/cam.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, WebSocket, Request
+from ...logic._grpc.grpc_pot import GRPC_Pot
+from ...logic._grpc.base import base_pb2
+from ..pot.pot import pot_connections
+import time
+
+router = APIRouter(
+    prefix="/cam"
+)
+
+
+@router.websocket("/{pot_code}")
+async def connect_pot(pot_code:str, request:Request, websocket:WebSocket):
+    grpc_response:base_pb2.Response = GRPC_Pot.pot_read(request.cookies["access_token"])
+    if not grpc_response.check:
+        raise "권한 x"
+    if not pot_code in pot_connections:
+        raise "화분 연결 x"
+    GRPC_Pot.pot_create(pot_code=websocket.headers["pot_code"])
+    await websocket.accept()
+
+    async def get_cam(websocket:WebSocket):
+        await websocket.send("cam")
+        return await websocket.receive_text()
+    
+    while True:
+        await websocket.send(get_cam(pot_connections[pot_code]))
+        time.sleep(1)
+
+    
