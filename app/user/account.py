1396eba89827a457f69211155813c9e2bd9afa53
byundojin <103907292+byundojin@users.noreply.github.com>
2024-04-17 18:08:14 +0900

    12

--- app/user/account.py
+++ app/user/account.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse as HTTP_Response
 from pydantic import BaseModel
 from logic._grpc.grpc_user import GRPC_User
 from logic._grpc.protos import base_pb2, User_db_pb2
+from typing import Annotated
  
 router = APIRouter(
     prefix="/account"
@@ -36,7 +37,17 @@ def post_login(body:UserBody):
     response.set_cookie("access_token", token.access_token.access)
     return response
 
-
+@router.post("/refresh")
+def refresh(refresh=Body(Annotated[str, None])):
+    if not refresh:
+        return HTTP_Response(content={}, status_code=404)
+    token:User_db_pb2.ResponseAccessToken = GRPC_User().refresh_token(refresh)
+    htc = token.response.http_code.split("/")
+    status_code = htc[0]
+    if len(htc) == 2:
+        message = htc[1]
+        return HTTP_Response(content={"message":message}, status_code=int(status_code))
+    return HTTP_Response(content={}, status_code=int(status_code))
 
 # ? 인증 만들 생각이였는데 뭐지
 # def authentication(func:function):
