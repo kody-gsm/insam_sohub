e8368a1c4a0472f13114edcbc93e756abb4b10b5
byundojin <103907292+byundojin@users.noreply.github.com>
2024-04-25 17:07:06 +0900

    123

--- app/pot/pot.py
+++ app/pot/pot.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, WebSocket
 from logic._grpc.grpc_pot import GRPC_Pot
+import asyncio
 
 router = APIRouter(
     prefix=""
@@ -14,6 +15,7 @@ async def connect_pot(websocket:WebSocket):
     await websocket.accept()
     pot_connections[websocket.headers["pot_code"]] = websocket
     
+
     GRPC_Pot().pot_create(pot_code=websocket.headers["pot_code"])
 
 
