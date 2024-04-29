abdb7f95c185f9ebb383fbf4676d053941589c6a
byundojin <103907292+byundojin@users.noreply.github.com>
2024-04-29 17:58:49 +0900

    1234

--- app/pot/pot.py
+++ app/pot/pot.py
@@ -6,12 +6,10 @@ router = APIRouter(
     prefix=""
 )
 
-pot_connections:dict[str, WebSocket]={}
+pot_connections:dict[str, WebSocket] = {}
 
 @router.websocket("/connect")
 async def connect_pot(websocket:WebSocket):
-    if websocket.headers["pot_code"] in pot_connections:
-        raise "pot_code 중복"
     await websocket.accept()
     pot_connections[websocket.headers["pot_code"]] = websocket
     GRPC_Pot().pot_create(pot_code=websocket.headers["pot_code"])
