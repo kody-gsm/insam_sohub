8d22f3e5ea5cfbb3b0ac406d79ad17f5a6b8f264
byundojin <103907292+byundojin@users.noreply.github.com>
2024-05-07 13:44:35 +0900

    qwe

--- app/user/image.py
+++ app/user/image.py
@@ -7,3 +7,7 @@ sched = BackgroundScheduler(timezone='Asia/Seoul')
 async def image_add():
     for pot_code in connect_socket.pot_sockets:
         await connect_socket.pot_sockets[pot_code].send(message="server#s4")
+
+@sched.scheduled_job(hour='12',minute='00', id='test')
+async def test():
+    print("test")
