bb238e9f0e0b4b19f22be88e07e5698f5e2374d9
byundojin <103907292+byundojin@users.noreply.github.com>
2024-05-20 13:35:40 +0900

    123

--- app/user/image.py
+++ app/user/image.py
@@ -3,10 +3,15 @@ from logic.socket import connect_socket
  
 sched = BackgroundScheduler(timezone='Asia/Seoul')
 
-@sched.scheduled_job(trigger="corn", hour='12',minute='00', id='image_add')
+@sched.scheduled_job(trigger="cronn", hour='12',minute='00', id='image_add')
 async def image_add():
     for pot_code in connect_socket.pot_sockets:
         await connect_socket.pot_sockets[pot_code].send(message="server#s4")
+
+# @sched.scheduled_job(trigger="interval", hour='1', id='feed_water')
+# async def feed_water():
+#     for pot_code in connect_socket.pot_sockets:
+#         await connect_socket.pot_sockets[pot_code].send(message="server#s4")
 
 from fastapi import APIRouter, WebSocket, Request, Header
 from fastapi.responses import JSONResponse as HTTP_Response
